7768548a26389b6e2562e8708c26a490fc821148
Matija Pevec <pevec@live.com>
2022-07-04 18:35:42 +0200

    attempt to fix gas price oracles on matic networks

--- src/app/shared/services/blockchain/gas.service.ts
+++ src/app/shared/services/blockchain/gas.service.ts
@@ -1,11 +1,12 @@
 import {Injectable} from '@angular/core'
-import {from, Observable, of, switchMap} from 'rxjs'
+import {Observable, ObservableInput, ObservedValueOf, of, switchMap} from 'rxjs'
 import {catchError, map} from 'rxjs/operators'
 import {PreferenceQuery} from '../../../preference/state/preference.query'
 import {Overrides} from 'ethers'
 import {HttpClient} from '@angular/common/http'
 import {SessionQuery} from '../../../session/state/session.query'
 import {parseUnits} from 'ethers/lib/utils'
+import {SignerService} from '../signer.service'
 import {ChainID} from '../../networks'
 
 @Injectable({
@@ -14,29 +15,33 @@ import {ChainID} from '../../networks'
 export class GasService {
   constructor(private preferenceQuery: PreferenceQuery,
               private sessionQuery: SessionQuery,
+              private signerService: SignerService,
               private http: HttpClient) {
   }
 
   get overrides(): Observable<Partial<Overrides>> {
-    return this.isEip1559Supported.pipe(
-      switchMap(isEip1559Supported => isEip1559Supported ?
-        of({}) : this.gasPriceOracleOverrides,
-      ),
+    return this.gasPriceOracleOverrides
+  }
+
+  withOverrides<T extends ObservableInput<any>>(fn: (overrides: Overrides) => T): Observable<ObservedValueOf<T>> {
+    return this.overrides.pipe(
+      switchMap(o => fn(o)),
     )
   }
 
   private get isEip1559Supported(): Observable<boolean> {
-    if (!this.sessionQuery.signer) return of(false)
-
-    return from(this.sessionQuery.signer.provider.getFeeData()).pipe(
+    return this.signerService.ensureNetwork.pipe(
+      switchMap(signer => signer.provider.getFeeData()),
       map(data => (data.maxFeePerGas != null || data.maxPriorityFeePerGas != null)),
     )
   }
 
   private get gasPriceOracleOverrides(): Observable<Partial<Overrides>> {
     switch (this.preferenceQuery.network.chainID) {
+      case ChainID.MATIC_MAINNET:
+        return this.maticGasStation('https://gasstation-mainnet.matic.network/v2')
       case ChainID.MUMBAI_TESTNET:
-        return this.maticGasStation('https://gasstation-mumbai.matic.today')
+        return this.maticGasStation('https://gasstation-mumbai.matic.today/v2')
       default:
         return of({})
     }
@@ -45,10 +50,11 @@ export class GasService {
   private maticGasStation(endpoint: string): Observable<Partial<Overrides>> {
     return this.http.get<GasStationRes>(endpoint).pipe(
       map(res => {
-        const gasPrice = Math.min(this.preferenceQuery.network.maxGasPrice, res.fast)
+        const fastRes = res.fast
         return {
-          gasPrice: parseUnits(gasPrice.toString(), 'gwei'),
-        }
+          maxFeePerGas: parseUnits(fastRes.maxFee.toFixed(9).toString(), 'gwei'),
+          maxPriorityFeePerGas: parseUnits(fastRes.maxPriorityFee.toFixed(9).toString(), 'gwei'),
+        } as Partial<Overrides>
       }),
       catchError(() => of({})),
     )
@@ -56,10 +62,19 @@ export class GasService {
 }
 
 interface GasStationRes {
-  safeLow: number;
-  standard: number;
-  fast: number;
-  fastest: number;
-  blockTime: number;
-  blockNumber: number;
+  safeLow: {
+    maxPriorityFee: number
+    maxFee: number
+  }
+  standard: {
+    maxPriorityFee: number
+    maxFee: number
+  }
+  fast: {
+    maxPriorityFee: number
+    maxFee: number
+  }
+  estimatedBaseFee: number
+  blockTime: number
+  blockNumber: number
 }
